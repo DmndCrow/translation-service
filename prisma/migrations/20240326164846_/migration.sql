4f4156ce2d03d30970d670e092682dbc9f75ed30
DmndCrow <aibek.ziyashev@nu.edu.kz>
2024-03-27 00:30:04 +0700

    create dockerfile and docker-compose

--- prisma/migrations/20240326164846_/migration.sql
+++ prisma/migrations/20240326164846_/migration.sql
@@ -10,6 +10,7 @@ CREATE TABLE "TranslationKey" (
 -- CreateTable
 CREATE TABLE "Language" (
     "id" SERIAL NOT NULL,
+    "key" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -29,10 +30,25 @@ CREATE TABLE "TranslationValue" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "TranslationKey_key_key" ON "TranslationKey"("key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Language_key_key" ON "Language"("key");
+
+-- CreateIndex
+CREATE INDEX "TranslationValue_languageId_idx" ON "TranslationValue"("languageId");
+
+-- CreateIndex
+CREATE INDEX "TranslationValue_translationKeyId_idx" ON "TranslationValue"("translationKeyId");
+
+-- CreateIndex
+CREATE INDEX "TranslationValue_languageId_translationKeyId_idx" ON "TranslationValue"("languageId", "translationKeyId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "TranslationValue_languageId_translationKeyId_key" ON "TranslationValue"("languageId", "translationKeyId");
 
 -- AddForeignKey
-ALTER TABLE "TranslationValue" ADD CONSTRAINT "TranslationValue_languageId_fkey" FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TranslationValue" ADD CONSTRAINT "TranslationValue_languageId_fkey" FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TranslationValue" ADD CONSTRAINT "TranslationValue_translationKeyId_fkey" FOREIGN KEY ("translationKeyId") REFERENCES "TranslationKey"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TranslationValue" ADD CONSTRAINT "TranslationValue_translationKeyId_fkey" FOREIGN KEY ("translationKeyId") REFERENCES "TranslationKey"("id") ON DELETE CASCADE ON UPDATE CASCADE;
