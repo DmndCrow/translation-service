52e628eb2201693528973d5cb62f3d98865bd19a
DmndCrow <aibek.ziyashev@nu.edu.kz>
2024-04-15 03:59:40 +0700

    add scroll to sidebar and hide button

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,12 +2,13 @@
 "use client";
 
 import { NestedObject } from "@/lib/models";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SidebarItem from "./sidebarItem";
 import { useSidebarContext } from "@/lib/contexts/sidebarContext";
 
 const Sidebar = () => {
   const { nestedKeys, fetchKeys } = useSidebarContext();
+  const [isDefaultOpen, setIsDefaultOpen] = useState(true);
 
   useEffect(() => {
     if (Object.keys(nestedKeys).length === 0) {
@@ -17,10 +18,21 @@ const Sidebar = () => {
 
   return (
     <aside>
-      <div className="bg-gray-800 w-64 h-full absolute">
+      <div className="bg-gray-800 w-64 h-full absolute overflow-auto">
+        <button
+          onClick={() => setIsDefaultOpen(!isDefaultOpen)}
+          className="text-gray-50 border-white border-2 p-1 bg-gray-800 text-left"
+        >
+          {isDefaultOpen ? "Свернуть" : "Раскрыть"}
+        </button>
+
         {Object.entries(nestedKeys).map(([key, value], i) => (
           <div key={i} className="p-1 text-white">
-            <SidebarItem label={key} children={value as NestedObject} />
+            <SidebarItem
+              isDefaultOpen={isDefaultOpen}
+              label={key}
+              children={value as NestedObject}
+            />
           </div>
         ))}
       </div>
